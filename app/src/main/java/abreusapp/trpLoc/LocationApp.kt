59e14a0e027418cb9f006f458ebfab805c8a44db
NewtonxD <carlosiabreup@gmail.com>
2024-04-07 08:31:54 -0400

    first

---
+++ app/src/main/java/abreusapp/trpLoc/LocationApp.kt
@@ -0,0 +1,23 @@
+package abreusapp.trpLoc
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+class LocationApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(
+                "location",
+                "STP - Localización",
+                NotificationManager.IMPORTANCE_LOW
+            )
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
---
+++ app/src/main/java/abreusapp/trpLoc/LocationClient.kt
@@ -0,0 +1,11 @@
+package abreusapp.trpLoc
+
+import android.location.Location
+import kotlinx.coroutines.flow.Flow
+
+interface LocationClient {
+
+    fun getLocationUpdates(interval: Long): Flow<Location>
+
+    class LocationException(message: String): Exception()
+}
